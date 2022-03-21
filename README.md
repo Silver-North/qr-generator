ac101c2ef38cdad3178d9a32a19c7529e615dbc7
north <self.norht.as@gmail.com>
2022-03-21 17:44:01 +0300

    init README

---
+++ README.md
@@ -0,0 +1,6 @@
+Simple QR-code generator
+For work need installing:
+ - PyQT5 (pip install pyqt5)
+ - PIL (pip install pillow)
+ - validators (pip install validators)
+ - qrcode (pip install qrcode)
--- main.py
+++ main.py
@@ -1,67 +1,3 @@
-# from qrcode.image.pure import PymagingImage
-# from qrcode import make, QRCode, constants
-# from qrcode.image.svg import SvgImage, SvgFragmentImage, SvgPathImage
-
-
-# data = input("Write URL or other data:  ")
-# factory = PymagingImage
-# # factory = SvgFragmentImage
-
-# qr = QRCode(version=1, error_correction=constants.ERROR_CORRECT_L, box_size=100, border=4, image_factory=factory)
-
-# qr.add_data(data)
-# qr.make(fit=True)
-
-# img = qr.make_image(fill_color="red", back_color="green")
-
-# filename = 'qr.png'
-# # img = make(data, image_factory=factory)
-# img.save(filename)
-
-
-
-
-# import qrcode
-# from qrcode.image.styledpil import StyledPilImage
-# from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
-# from qrcode.image.styles.colormasks import RadialGradiantColorMask, ImageColorMask, SquareGradiantColorMask, SolidFillColorMask, HorizontalGradiantColorMask, VerticalGradiantColorMask
-
-# qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L)
-# qr.add_data('Some data')
-
-# img_1 = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer(), color_mask=SquareGradiantColorMask())
-# img_1 = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer(), color_mask=SquareGradiantColorMask(), embeded_image_path="animevost.png")
-# img_1.save('qr-test.png')
-# , module_drawer=RoundedModuleDrawer())
-# img_2 = qr.make_image(image_factory=StyledPilImage, color_mask=RadialGradiantColorMask())
-# img_2 = qr.make_image(image_factory=StyledPilImage, color_mask=ImageColorMask(), embeded_image_path='animevost.png')
-# img_2.save('qr-test2.png')
-# img_3 = qr.make_image(image_factory=StyledPilImage, embeded_image_path="/path/to/image.png")
-
-
-
-
-# from MyQR import myqr as mq
-# mq.run(words = 'https://www.animevost.org', version = 6, picture = 'animevost.png', colorized = True, save_name = 'topcoder.png')
-
-
-
-
-# from MyQr import myqr
-# from myqr import run
-
-# run('https://animevost.org', save_name="av.png")
-
-
-
-
-
-
-
-
-
-
-
 #!/usr/bin/env python3
 
 
@@ -87,7 +23,7 @@ class mywindow(QtWidgets.QMainWindow):
         # self.setGeometry(900, 55, 400, 300)
 
         self.current_path = path.dirname(path.realpath(__file__))
-        
+
         self.ui.toolButton_2.setIcon(QIcon(f'{self.current_path}/spinner.png'))
         self.ui.toolButton_3.setIcon(QIcon(f'{self.current_path}/diskette.png'))
         self.ui.toolButton.clicked.connect(self.getPathFile)
@@ -97,7 +33,7 @@ class mywindow(QtWidgets.QMainWindow):
 
     def generate(self, text):
         filename = 'qr.png'
-            
+
         img = make(text)
         img.save(filename)
 
@@ -136,7 +72,7 @@ class mywindow(QtWidgets.QMainWindow):
         pixmap = QPixmap(f'{self.current_path}/qr.png')
         self.ui.label_3.setPixmap(pixmap)
 
-        
+
     def textQR(self):
         if self.ui.toolBox.currentIndex() == 0:
             text = self.ui.lineEdit.text()
@@ -186,5 +122,5 @@ app = QtWidgets.QApplication([])
 application = mywindow()
 
 application.show()
- 
+
 exit(app.exec())
